7167683d37a614b7d4bd209a736ecbfbca3041c3
keshavprasadms <msknext@gmail.com>
2020-06-05 12:31:31 +0530

    fix: skip test cases

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ name=adminutil
 node=$2
 org=$3
 
-./gradlew build --stacktrace
+./gradlew clean build -x test --stacktrace
 
 docker build -f ./Dockerfile --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${build_tag} .
 echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\", \"node_name\" : \"$node\"} > metadata.json
