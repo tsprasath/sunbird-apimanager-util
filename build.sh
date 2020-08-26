e0236985de0613324d4bc0691bc5bfc51e0a76bd
keshavprasadms <msknext@gmail.com>
2020-08-26 19:14:59 +0530

    fix: adding back test cases check

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ name=adminutil
 node=$2
 org=$3
 
-./gradlew build -x test --stacktrace
+./gradlew build --stacktrace
 
 docker build -f ./Dockerfile --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${build_tag} .
 echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\", \"node_name\" : \"$node\"} > metadata.json
